5e1e26608ee0c9f59a0d5a308f3199b5137f06c6
AnandCodeX <Anandrtiwari@live.com>
2024-08-23 11:02:33 +0100

    Boiler plate

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Contactus from './components/Contactus';
+import Footer from './components/Footer';
+import Gallary from './components/Gallary';
+import Main from './components/Main';
+import Navebar from './components/Navebar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    <Navebar/>
+    <Main/>
+    <Gallary/>
+    <About/>
+    <Contactus/>
+    <Footer/>
     </div>
   );
 }
