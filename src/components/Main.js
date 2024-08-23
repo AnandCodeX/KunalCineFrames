37842a519ca75c7e70f2191fbd751fa2c273a701
AnandCodeX <Anandrtiwari@live.com>
2024-08-23 10:57:12 +0100

    Boilet plate

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div>
+      Main Division
+    </div>
+  )
+}
+
+export default Main
---
+++ src/components/Navebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navebar = () => {
+  return (
+    <div>
+      Navebar
+    </div>
+  )
+}
+
+export default Navebar
